ad8eb4ec0dda2cfaddc8e206d932990307b3e85f
Tom Berghuis <thomas.berghuis@gmail.com>
2017-03-22 17:10:05 +0700

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ class App extends Component {
         TwitchService.populateChannelList(this.channelList);
         //console.log("channel list 0",this.channelList[0].name);
         //console.log("channel list",this.channelList);
-        
+
         // debugging
         //window.channelList = this.channelList;
     }
@@ -32,23 +32,24 @@ class App extends Component {
             <div class="container">
                 <h1>FCC Twitch Streamers</h1>
                 <div>
-                    <span role="button" onClick={()=>this.showStreaming = !this.showStreaming}>
-                        <input type="checkbox" 
+                    <span role="button" onClick={() => this.showStreaming = !this.showStreaming}>
+                        <input type="checkbox"
                             checked={this.showStreaming} /> Streaming</span>
-                    <span role="button" onClick={()=>this.showOffline = !this.showOffline}>
-                        <input type="checkbox" 
+                    <span role="button" onClick={() => this.showOffline = !this.showOffline}>
+                        <input type="checkbox"
                             checked={this.showOffline} /> Offline</span>
                 </div>
                 <div>
-                    {this.channelList.map((channel)=>{
+                    {this.channelList.filter((channel) => {
+                        return (this.showOffline && !channel.streaming)
+                            || (this.showStreaming && channel.streaming);
+                    }).map((channel) => {
                         return (
-                            <div>
+                            <div class={"twitch-channel " + (channel.streaming ? "streaming":"offline")} >
                                 {/* highlight green streaming, red offline */}
-                                <img class="logo" src={channel.logo} />
-                                    
-                                {channel.name} 
-                                {channel.game}
-                                
+                                <div><img class="logo" src={channel.logo} /></div>
+                                <div><a href={channel.url} target="_blank">{channel.name}</a></div>
+                                <div class="game">{channel.game}</div>
                             </div>
                         );
                     })}
--- src/services/TwitchService.js
+++ src/services/TwitchService.js
@@ -1,7 +1,7 @@
-import Channel from '../stores/Channel.js';
+//import Channel from '../stores/Channel.js';
 const jsonp = require('jsonp');
 
-const channelNames = ["ESL_SC2", "pokemontcg", "freecodecamp", "adobe", "Nightblue3", "somuchmonsters", "williamchyr", "triplegzgaming", "geekandgamergirl"];
+const channelNames = ["ESL_SC2", "pokemontcg", "freecodecamp", "adobe", "Nightblue3", "somuchmonsters", "williamchyr", "triplegzgaming", "geekandgamergirl", "artemis"];
 
 export default {
     populateChannelList(channelList) {
@@ -10,16 +10,29 @@ export default {
                 if (err) {
                     console.error(err.message);
                 } else {
-                    let channel = new Channel();
+                    //let channel = new Channel();
+                    let channel = {};
                     channel.name = name;
                     channel.streaming = !!data.stream;
-                    channel.game = data.stream ? data.stream.game : "offline";
+
                     //channelList.push(channel);
                     jsonp('https://wind-bow.gomix.me/twitch-api/channels/' + name, null, function (err, data) {
                         if (err) {
                             console.error(err.message);
                         } else {
                             channel.logo = data.logo;
+                            channel.url = data.url;
+
+                            if (channel.streaming) {
+                                channel.game = data.game + ": " + data.status;
+                                // TODO if channel.game is too long truncate
+
+
+                            } else {
+                                channel.game = "offline";
+                            }
+
+
                             channelList.push(channel);
                         }
                     });
@@ -36,4 +49,3 @@ export default {
         console.log('bar');
     }
 };
-

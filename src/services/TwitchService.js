a3ddc50608a6562b129b35fad9e2fa293f2ee851
Tom Berghuis <thomas.berghuis@gmail.com>
2017-03-23 12:26:51 +0700

    .

--- src/services/TwitchService.js
+++ src/services/TwitchService.js
@@ -2,17 +2,23 @@ const jsonp = require('jsonp');
 
 const channelNames = ["ESL_SC2", "pokemontcg", "freecodecamp", "adobe", "Nightblue3", "somuchmonsters", "williamchyr", "triplegzgaming", "geekandgamergirl", "artemis","comster404","SaltyBet"];
 
+const client_id = '3pddx4dsjwwmypmi8xts7vqdrmbjuz';
+
+
 export default {
     populateChannelList(channelList) {
         channelNames.forEach((name) => {
-            jsonp('https://wind-bow.gomix.me/twitch-api/streams/' + name, null, function (err, data) {
+            //$.getJSON('https://api.twitch.tv/kraken/streams/' + streamers[i] + "?client_id="+ KEY
+            //jsonp('https://wind-bow.gomix.me/twitch-api/streams/' + name, null, function (err, data) {
+            jsonp('https://api.twitch.tv/kraken/streams/' + name + '?client_id=' + client_id, null, function (err, data) {
                 if (err) {
                     console.error(err.message);
                 } else {
                     let channel = {};
                     channel.name = name;
                     channel.streaming = !!data.stream;
-                    jsonp('https://wind-bow.gomix.me/twitch-api/channels/' + name, null, function (err, data) {
+                    //jsonp('https://wind-bow.gomix.me/twitch-api/channels/' + name, null, function (err, data) {
+                    jsonp('https://api.twitch.tv/kraken/channels/' + name + '?client_id=' + client_id, null, function (err, data) {
                         if (err) {
                             console.error(err.message);
                         } else {
